ae996628c223412814298d69409b30b484c200a7
yuwantha98 <yuwantharavihara123@gmail.com>
2026-08-19 14:08:18 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
+import Search from "./pages/Search";
 
 function App() {
   return (
@@ -11,10 +13,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
