992d3756b0628ce7c7bd82d46739ba67a7fbe0f2
diluni25 <diluniwijewardana25@gmail.com>
2026-08-30 15:40:28 +0530

    Update Home and Shop pages

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Shop from "./Shop";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </Router>
+  );
+}
