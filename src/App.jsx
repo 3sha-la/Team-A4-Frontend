3c239b52bf1cf75b8a75497c12a29a8f73bc94c1
diluni25 <diluniwijewardana25@gmail.com>
2026-08-30 16:58:07 +0530

    Update routes to include Home and Shop components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Shop from "./Shop";
 import Cart from "./Cart";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Cart />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
